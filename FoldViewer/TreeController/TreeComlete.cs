8eca8147ed7e276e3eed302a4b53d6299b45f75b
Алексей Кошкин <alekseykoshkin@rambler.ru>
2016-09-27 15:12:01 +0300

    Добавил возможность полного обхода дерева директорий

--- FoldViewer/TreeController/TreeComlete.cs
+++ FoldViewer/TreeController/TreeComlete.cs
@@ -12,42 +12,93 @@ namespace FoldViewer.TreeController
         public void TreeStartComplete(Tree rootTree)
         {
             DriveInfo[] myDrives = DriveInfo.GetDrives();
-           var directory =  myDrives.Where(x => x.IsReady)
+            var directory = myDrives.Where(x => x.IsReady)
                 .Select(x => new DirectoryInfo(x.ToString()).EnumerateDirectories().ToList()
-           /*.Select(y => y)
-           .Where(y => (y.Attributes & FileAttributes.Hidden) == 0)*/)
-           .ToList();
-            var dir = myDrives.Where(x => (x.IsReady) && (x.Name != "C:\\" ) ).ToList();
-            var dirNodes = dir.Select(x => new TreeNode(null,new DirectoryInfo(x.ToString()))).ToList();
+                    /*.Select(y => y)
+            .Where(y => (y.Attributes & FileAttributes.Hidden) == 0)*/)
+                .ToList();
+            var dir = myDrives.Where(x => x.IsReady).ToList();
+            var dirNodes = dir.Select(x => new TreeNode(null, new DirectoryInfo(x.ToString()))).ToList();
             rootTree.ChildNodes = dirNodes;
             // directory
             foreach (var subDir in rootTree.ChildNodes)
             {
-               SetTreeNode(subDir);
+                SetTreeNode(subDir);
             }
-           
 
 
+
+        }
+
+        public void TestTestAccess(TreeNode treeNode)
+        {
+            Stack<string> dirs = new Stack<string>();
+            dirs.Push(treeNode.FolderFullPath);
+
+            while (dirs.Count > 0)
+            {
+                string currentDirPath = dirs.Pop();
+                try
+                {
+                    string[] subDirs = Directory.GetDirectories(currentDirPath);
+                    foreach (string subDirPath in subDirs)
+                    {
+                        dirs.Push(subDirPath);
+                    }
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    continue;
+                }
+                catch (DirectoryNotFoundException)
+                {
+                    continue;
+                }
+            }
         }
 
         public void SetTreeNodeInfo(TreeNode treeNode)
         {
-
-            treeNode.ChildNodes =
-                treeNode.TreeNodeDirectoryInfo.EnumerateDirectories().Select(x => new TreeNode(treeNode, x)).ToList();
-            treeNode.FolderFullPath = treeNode.TreeNodeDirectoryInfo.FullName;
-            treeNode.FolderName = treeNode.TreeNodeDirectoryInfo.Name;
+            try
+            {
+                if ((treeNode.TreeNodeDirectoryInfo.Attributes & FileAttributes.NotContentIndexed) == 0)
+                {
+                    treeNode.ChildNodes =
+                        treeNode.TreeNodeDirectoryInfo.EnumerateDirectories()
+                            .Select(x => new TreeNode(treeNode, x))
+                            .ToList();
+                    treeNode.FolderFullPath = treeNode.TreeNodeDirectoryInfo.FullName;
+                    treeNode.FolderName = treeNode.TreeNodeDirectoryInfo.Name;
+                }
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                throw e;
+            }
         }
 
         public void SetTreeNode(TreeNode treeNode)
         {
-           // if ((treeNode.TreeNodeDirectoryInfo.Attributes & FileAttributes.Hidden) == 0)
+            // if ((treeNode.TreeNodeDirectoryInfo.Attributes & FileAttributes.Hidden) == 0)
             if (treeNode.TreeNodeDirectoryInfo.Attributes != FileAttributes.System)
             {
-                SetTreeNodeInfo(treeNode);
-                foreach (var childNodes in treeNode.ChildNodes)
+                if ((treeNode.TreeNodeDirectoryInfo.Attributes & FileAttributes.NotContentIndexed) == 0)
                 {
-                    SetTreeNode(childNodes);
+                    try
+                    {
+
+
+                        SetTreeNodeInfo(treeNode);
+                        // TestTestAccess(treeNode);
+                        foreach (var childNodes in treeNode.ChildNodes)
+                        {
+                            SetTreeNode(childNodes);
+                        }
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        
+                    }
                 }
             }
         }
